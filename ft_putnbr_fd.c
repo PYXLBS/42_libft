2568e83e01b541aa261a8433e915c958301858ef
Paula Berthaud <polaberto@protonmail.com>
2023-05-19 22:11:50 +0200

    Add files via upload

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,50 +6,45 @@
 /*   By: pyxlbs <pyxlbs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 16:34:31 by pabertha          #+#    #+#             */
-/*   Updated: 2023/05/19 20:17:56 by pyxlbs           ###   ########.fr       */
+/*   Updated: 2023/05/19 22:10:19 by pyxlbs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	ft_putchar_fd(char c, int fd)
+{
+	write (fd, &c, 1);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-
-}
-
-int	main(void)
-{
-	return (0);
-}
-
-PISCINE
-
-void	ft_putchar(char c)
-{
-	write (1, &c, 1);
-}
-
-void	ft_putnbr(int nb)
-{
-	if (nb == -2147483648)
+	if (n == -2147483648)
 	{
-		ft_putchar('-');
-		ft_putchar('2');
-		ft_putnbr(147483648);
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		ft_putnbr_fd(147483648, fd);
 	}
-	else if (nb < 0)
+	else if (n < 0)
 	{
-		ft_putchar('-');
-		nb = -nb;
-		ft_putnbr(nb);
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
 	}
-	else if (nb > 9)
+	else if (n > 9)
 	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
 	else
 	{
-		ft_putchar(nb + 48);
+		ft_putchar_fd(n + 48, fd);
 	}
 }
+
+/*
+int	main(void)
+{
+	ft_putnbr_fd();
+	return (0);
+}
+*/
