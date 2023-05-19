9a805d10cd043fad5118e7e9a75c3b713e5e5531
Paula Berthaud <polaberto@protonmail.com>
2023-05-19 17:23:10 +0200

    Add files via upload

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pabertha <pabertha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pyxlbs <pyxlbs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:17:08 by pabertha          #+#    #+#             */
-/*   Updated: 2023/05/11 17:05:22 by pabertha         ###   ########.fr       */
+/*   Updated: 2023/05/19 17:19:04 by pyxlbs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_atoi(const char *nptr)
 	{
 		while ((nptr[c] > 8 && nptr[c] < 14) || (nptr[c] == 32))
 			c++;
-		while (nptr[c] == 43 || nptr[c] == 45)
+		if (nptr[c] == 43 || nptr[c] == 45)
 		{
 			if (nptr[c] == 45)
 				d = -d;
@@ -33,7 +33,7 @@ int	ft_atoi(const char *nptr)
 		}
 		while (nptr[c] > 47 && nptr[c] < 58)
 		{
-			e = e * 10 + (nptr[c] - 48);
+			e = (e * 10) + (nptr[c] - 48);
 			c++;
 		}
 		return (d * e);
@@ -44,6 +44,19 @@ int	ft_atoi(const char *nptr)
 int	main(void)
 {
 	printf("%d\n", ft_atoi(" ---+--+1234ab567"));
+	printf("%d\n", ft_atoi("---+--+1234ab567"));
+	printf("%d\n", ft_atoi("-+1234ab567"));
+	printf("%d\n", ft_atoi("+1234ab567"));
+	printf("%d\n", ft_atoi("++1234ab567"));
+	printf("%d\n", ft_atoi("1234ab567"));
+	printf("%d\n", ft_atoi("qwerty1234ab567"));
+	printf("%s\n", "separation");
 	printf("%d\n", atoi(" ---+--+1234ab567"));
+	printf("%d\n", atoi("---+--+1234ab567"));
+	printf("%d\n", atoi("-+1234ab567"));
+	printf("%d\n", atoi("+1234ab567"));
+	printf("%d\n", atoi("++1234ab567"));
+	printf("%d\n", atoi("1234ab567"));
+	printf("%d\n", atoi("qwerty1234ab567"));
 	return (0);
 }
