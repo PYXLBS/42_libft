04ddd63bafc8bb9925a0a13a6817f1b7b4fd0957
Paula Berthaud-Salmon <polaberto@protonmail.com>
2023-05-23 23:17:36 +0200

    Add files via upload

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pabertha <pabertha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pyxlbs <pyxlbs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:24:22 by pabertha          #+#    #+#             */
-/*   Updated: 2023/05/11 17:06:02 by pabertha         ###   ########.fr       */
+/*   Updated: 2023/05/23 23:16:05 by pyxlbs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,23 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
+	size_t i;
 
+	i = 0;
+	while (src[i] != '\0')
+	{
+		if (size > i + 1)
+			dst[i] = src[i];
+		i++;
+	}
+	if (size != 0)
+		dst[i] = '\0';
+	return (i);
 }
 
+/*
 int	main(void)
 {
 	return (0);
 }
+*/
