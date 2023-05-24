6ba6fff2d16de94b8ddaa96c56e0b693bfac98ce
Paula Berthaud-Salmon <polaberto@protonmail.com>
2023-05-24 13:26:51 +0200

    Add files via upload

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pabertha <pabertha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pyxlbs <pyxlbs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:24:22 by pabertha          #+#    #+#             */
-/*   Updated: 2023/05/11 17:06:12 by pabertha         ###   ########.fr       */
+/*   Updated: 2023/05/24 13:24:38 by pyxlbs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,23 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-
+	int	d;
+	
+	d = 0;
+	while (s[d] != '\0')
+		d++;
+	while (d >= 0)
+	{
+		if (s[d] == (char)c)
+			return ((char *)&s[d]);
+		d--;
+	}
+	return (0);
 }
 
+/*
 int	main(void)
 {
 	return (0);
 }
+*/
