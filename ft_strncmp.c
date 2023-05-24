78f7aefe65b5ccca30d8ac0e5c03456cbaa70b41
Paula Berthaud-Salmon <polaberto@protonmail.com>
2023-05-24 16:18:55 +0200

    Add files via upload

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pabertha <pabertha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pyxlbs <pyxlbs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:24:22 by pabertha          #+#    #+#             */
-/*   Updated: 2023/05/11 17:06:07 by pabertha         ###   ########.fr       */
+/*   Updated: 2023/05/24 16:17:00 by pyxlbs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,21 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	size_t	i;
 
+	i = 0;
+	while (((s1[i] + (s2[i] != '\0')) && i < n))
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
 }
 
+/*
 int	main(void)
 {
 	return (0);
 }
+*/
