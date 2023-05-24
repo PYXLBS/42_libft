712b81ee6baa51e4ab9d6d817a53726221bc158b
Paula Berthaud-Salmon <polaberto@protonmail.com>
2023-05-24 16:58:03 +0200

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,21 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pabertha <pabertha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pyxlbs <pyxlbs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:24:22 by pabertha          #+#    #+#             */
-/*   Updated: 2023/05/11 17:06:10 by pabertha         ###   ########.fr       */
+/*   Updated: 2023/05/24 16:56:26 by pyxlbs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#include <stddef.h>
+
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
+	size_t b;
+	size_t l;
 
+	b = 0;
+	l = 0;
+	while (big[b] != '\0')
+	{
+		if (little[l] == '\0')
+			return ((char *)big);
+		while (len > b)
+		{
+			while (big[b + l] == little[l] && len > b + l)
+			{
+				if (little[l + 1] == '\0')
+					return ((char *)&big[b]);
+				l++;
+			}
+		b++;
+		}
+	}
+	return (0);
 }
 
+/*
 int	main(void)
 {
 	return (0);
 }
+*/
