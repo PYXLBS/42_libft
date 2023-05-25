5ed074257e9f6452dad7dc535bdc85faf44bad8b
Paula Berthaud-Salmon <polaberto@protonmail.com>
2023-05-25 13:45:33 +0200

    Add files via upload

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pabertha <pabertha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pyxlbs <pyxlbs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 17:29:30 by pabertha          #+#    #+#             */
-/*   Updated: 2023/05/11 17:31:30 by pabertha         ###   ########.fr       */
+/*   Updated: 2023/05/25 13:33:37 by pyxlbs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,25 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	char	*str;
 
+	str = malloc(len + 1);
+	if (str == NULL)
+		return (0);
+	str[len] = '\0';
+	while (len--)
+	{
+		if (s[start + len] == '\0')
+			str[len] = '\0';
+		else
+			str[len] = s[start + len];
+	}
+	return (str);
 }
 
+/*
 int	main(void)
 {
 	return (0);
 }
+*/
