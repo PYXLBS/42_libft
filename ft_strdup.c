cb33b718d2b77f7ff03a506d9650d536631e0f2a
Paula Berthaud-Salmon <polaberto@protonmail.com>
2023-05-27 12:38:50 +0200

    Add files via upload

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,30 +6,30 @@
 /*   By: pyxlbs <pyxlbs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:24:22 by pabertha          #+#    #+#             */
-/*   Updated: 2023/05/25 18:56:17 by pyxlbs           ###   ########.fr       */
+/*   Updated: 2023/05/27 12:36:12 by pyxlbs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// char	*ft_strdup(const char *s)
-// {
-// 	int	t;
-// 	char	*u;
+char	*ft_strdup(const char *s)
+{
+	int	t;
+	char	*u;
 
-// 	t = 0;
-// 	u = malloc(t + 1);
-// 	if (u == NULL)
-// 		return (0);
-// 	while (s[t] != '\0')
-// 	{
-// 		u[t] = s[t];
-// 		t++;
-// 	}
-// 	if (u[t] == '\0')
-// 		return (u);
-// 	return (0);
-// }
+	t = 0;
+	u = malloc(t + 1);
+	if (u == NULL)
+		return (0);
+	while (s[t] != '\0')
+	{
+		u[t] = s[t];
+		t++;
+	}
+	if (u[t] == '\0')
+		return (u);
+	return (0);
+}
 
 /*
 int	main(void)
