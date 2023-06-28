589dbfecdb36c22605f04273677b31cd8cc23d03
Paula Berthaud-Salmon <polaberto@protonmail.com>
2023-06-28 16:32:47 +0200

    Add files via upload

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: pabertha <pabertha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 17:47:27 by pabertha          #+#    #+#             */
-/*   Updated: 2023/06/25 19:25:01 by pabertha         ###   ########.fr       */
+/*   Updated: 2023/06/28 16:31:34 by pabertha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	ft_lstsize(t_list *lst)
 	int	i;
 
 	i = 0;
-	if (lst == NULL)
-		return (0);
 	while (lst != NULL)
 	{
 		lst = lst->next;
